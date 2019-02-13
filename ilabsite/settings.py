fe553e4980959398950b85819e144aae195119a0
trungtranle <trung.tranle13@gmail.com>
2019-02-13 20:09:54 +0700

    remove redis cache

--- ilabsite/settings.py
+++ ilabsite/settings.py
@@ -189,13 +189,3 @@ WAGTAILADMIN_RICH_TEXT_EDITORS = {
         }
     }
 }
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://localhost:6379',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
